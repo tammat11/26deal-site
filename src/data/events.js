c0028dc0ef1e698c388d3c6b44ea35dad782a5bd
tammat11 <laybleee1@gmail.com>
2026-02-16 14:11:44 +0500

    Update events via Admin Panel

--- src/data/events.js
+++ src/data/events.js
@@ -1,18 +1,22 @@
 export const events = [
     {
-        id: 1,
-        title: "Совет Директоров",
-        description: "Стратегическая сессия по масштабированию бизнесов резидентов.",
-        date: "2026-03-15",
-        photo: "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?auto=format&fit=crop&q=80&w=800",
-        participants: []
+        "id": 1,
+        "title": "Совет Директоров",
+        "description": "Стратегическая сессия по масштабированию бизнесов резидентов.",
+        "date": "2026-03-15",
+        "photo": "https://images.unsplash.com/photo-1542744173-8e7e53415bb0?auto=format&fit=crop&q=80&w=800",
+        "participants": [
+            "Ларион Лян",
+            "Тимур Нуртаев",
+            "Болат Джанабаев"
+        ]
     },
     {
-        id: 2,
-        title: "Бизнес-форум 26 Deal",
-        description: "Ежегодная встреча лидеров рынка и обмен опытом.",
-        date: "2026-04-20",
-        photo: "https://images.unsplash.com/photo-1511578314322-379afb476865?auto=format&fit=crop&q=80&w=800",
-        participants: []
+        "id": 2,
+        "title": "Бизнес-форум 26 Deal",
+        "description": "Ежегодная встреча лидеров рынка и обмен опытом.",
+        "date": "2026-04-20",
+        "photo": "https://images.unsplash.com/photo-1511578314322-379afb476865?auto=format&fit=crop&q=80&w=800",
+        "participants": []
     }
 ];
